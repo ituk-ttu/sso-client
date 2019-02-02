838b10dacf140e6fa80f36d129e2599f801da847
Mark-Kristjan Markson <mamark@ttu.ee>
2019-02-02 15:22:13 +0200

    Hei

---
+++ src/main/java/ee/ituk/SsoTestClient/SsoaSecurityConfig.java
@@ -0,0 +1,23 @@
+package ee.ituk.SsoTestClient;
+
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableOAuth2Sso
+@Configuration
+public class SsoaSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+
+}
